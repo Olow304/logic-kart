a2cb8a3a55ecbdecf11c0ade7ccb682c6c6a5c5e
Saleban Olow <olow304@gmail.com>
2018-12-03 15:34:41 -0500

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,4 +1,5 @@
 module.exports = {
-    DB: "mongodb://logic:logic123@ds123454.mlab.com:23454/logic",
+    //DB: "mongodb://<username>:<password>@xxxxxx.mlab.com:23454/<db-name>",
+    DB: "localhost:27017/logic",
     sect: "!logic!8*Xccv"
 }
